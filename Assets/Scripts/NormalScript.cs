2e64e9908a2bbc651eba6309561a65cc281ecc93
池谷賀樹 <ikegaya@ikegayagaju-no-MacBook-Pro.local>
2017-01-08 16:02:12 +0900

    first commit

---
+++ Assets/Scripts/NormalScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NormalScript : MonoBehaviour {
+
+	public GameManager gameManager;
+
+	void OnCollisionEnter () {
+		Destroy (gameObject);
+		gameManager.score += 10;
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	float accel = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody> ().AddForce (transform.right * Input.GetAxisRaw ("Horizontal") * accel, ForceMode.Impulse);
+	}
+}
